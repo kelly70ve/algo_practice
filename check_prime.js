2dda72406fa5f3e713dd2f30568329c22adb39ce
Kelly Gowing <Gowing.kelly@yahoo.com>
2021-04-05 13:25:37 -0700

    add thatjsdude solution

--- check_prime.js
+++ check_prime.js
@@ -1,9 +1,26 @@
 function checkPrime(num) {
   if (num % 2 === 0) {
-    return false;
+    return false; // This is only checking for even vs odd
   } else {
     return true;
   }
 }
 
-console.log(checkPrime(11));
+checkPrime(237); // true
+
+// Source: http://thatjsdude.com/interview/js1.html
+// A prime number is only divisible by itself and 1. So, i will run a while loop and increase by 1.
+
+function isPrime(n) {
+  var divisor = 2;
+
+  while (n > divisor) {
+    if (n % divisor == 0) {
+      return false;
+    } else divisor++;
+  }
+  return true;
+}
+
+isPrime(137); // true
+isPrime(237); // false
